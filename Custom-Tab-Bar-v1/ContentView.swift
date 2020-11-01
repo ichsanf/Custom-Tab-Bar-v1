07b4dca850dfec92e2e511c2923c83c9da93981d
Achmad Ichsan Fauzi <ichsan.fauzi@icloud.com>
2020-11-01 23:55:59 +0700

    contentView (List)

--- Custom-Tab-Bar-v1/ContentView.swift
+++ Custom-Tab-Bar-v1/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedTab = "Home"
+//    @StateObject var modelData = ModelView()
+    @Namespace var animation
+    var edges = UIApplication.shared.windows.first?.safeAreaInsets
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            GeometryReader {_ in
+                ZStack {
+                    
+                    // Tabs
+                    NavigationView{
+                        ScrollView {
+                            ForEach(1...25, id: \.self) { i in
+                                NavigationLink(
+                                    destination: Text("Home Data \(i)")) {
+                                    
+                                    VStack(alignment: .leading) {
+                                        Text("Home Data \(i)")
+                                            .padding()
+                                            .foregroundColor(.black)
+                                        
+                                        Divider()
+                                    }
+                                }
+                            }
+                            .padding()
+                        }
+                        .navigationBarHidden(true)
+                    }
+                }
+            }
+        }
     }
 }
 
