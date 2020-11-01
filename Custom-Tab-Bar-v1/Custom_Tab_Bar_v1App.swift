09216a22daf516603324060ed3395fd1fd506f8a
Achmad Ichsan Fauzi <ichsan.fauzi@icloud.com>
2020-11-01 23:45:51 +0700

    Initial Commit

---
+++ Custom-Tab-Bar-v1/Custom_Tab_Bar_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Tab_Bar_v1App.swift
+//  Custom-Tab-Bar-v1
+//
+//  Created by Achmad Ichsan Fauzi on 01/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Tab_Bar_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
